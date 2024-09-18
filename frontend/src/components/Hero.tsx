9a1ca2ed2ecb14ae8c63ebb9c027abe634dcb209
HasangaW <hasangawr@gmail.com>
2024-09-18 17:25:29 +0530

    Style login form

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,25 +2,44 @@ import { Box, Typography } from '@mui/material';
 
 const Hero = () => {
   return (
-    <Box>
-      <Box>
-        <Typography>Welcome to PlanCraftr</Typography>
-      </Box>
-      <Box>
-        <Typography>
-          Welcome to your personal productivity hub! Plan your goals, break them
-          into tasks, and track your progress—all in one place. Let’s achieve
-          more, step by step!
-        </Typography>
-      </Box>
-      <Box>
+    <Box
+      sx={{
+        background:
+          'linear-gradient(109.6deg, rgb(20, 30, 48) 11.2%, rgb(36, 59, 85) 91.1%);',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box sx={{ flex: '0 1 49rem' }}>
         <Box>
-          <Typography>
-            "Success is the sum of small efforts, repeated day in and day out."
+          <Typography
+            variant="h2"
+            sx={{ fontWeight: 'bold', color: '#ffffff' }}
+          >
+            Welcome to PlanCraftr
           </Typography>
         </Box>
         <Box>
-          <Typography>– Robert Collier</Typography>
+          <Typography sx={{ color: '#6C737F' }}>
+            Welcome to your personal productivity hub! Plan your goals, break
+            them into tasks, and track your progress—all in one place. Let’s
+            achieve more, step by step!
+          </Typography>
+        </Box>
+        <Box>
+          <Box sx={{ marginTop: '1.5rem' }}>
+            <Typography sx={{ color: '#6C737F' }}>
+              "Success is the sum of small efforts, repeated day in and day
+              out."
+            </Typography>
+          </Box>
+          <Box>
+            <Typography sx={{ color: '#6C737F', fontStyle: 'italic' }}>
+              – Robert Collier
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </Box>
--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,16 +1,17 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { Box, Button, Link, Paper, TextField, Typography } from '@mui/material';
 
 const LoginForm = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const [error, setError] = useState<string | null>(null);
+  //const [error, setError] = useState<string | null>(null);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!email || !password) {
-      setError('Please enter both email and password.');
+      //setError('Please enter both email and password.');
       return;
     }
 
@@ -19,7 +20,7 @@ const LoginForm = () => {
     setEmail('');
     setPassword('');
 
-    setError(null);
+    //setError(null);
 
     try {
       const response = await axios.post(
@@ -34,37 +35,91 @@ const LoginForm = () => {
   };
 
   return (
-    <>
-      <h1>Welcome to PlanCraftr</h1>
-      <div className="container">
-        <div className="login">
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="email">Email:</label>
-              <input
-                id="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Password:</label>
-              <input
-                id="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <button type="submit">Login</button>
-          </form>
-        </div>
-      </div>
-    </>
+    <Box
+      sx={{
+        height: '100vh',
+        flex: '0 0 37.5rem',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Paper elevation={3} sx={{ padding: '2rem', margin: '0 4rem' }}>
+        <Box sx={{ display: 'flex' }}>
+          <Box sx={{ marginRight: '10px' }}>
+            <svg
+              id="logo-35"
+              width="50"
+              height="39"
+              viewBox="0 0 50 39"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {' '}
+              <path
+                d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z"
+                className="ccompli1"
+                fill="#007AFF"
+              ></path>{' '}
+              <path
+                d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
+                className="ccustom"
+                fill="#312ECB"
+              ></path>{' '}
+            </svg>
+          </Box>
+          <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+            Plan<span style={{ color: '#6366F1' }}>Craftr</span>
+          </Typography>
+        </Box>
+        <Box sx={{ marginTop: '2rem' }}>
+          <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+            Log in
+          </Typography>
+          <Typography sx={{ color: '#6C737F' }}>
+            Don't have an account?{' '}
+            <Link sx={{ color: '#6366F1', cursor: 'pointer' }} underline="none">
+              Register
+            </Link>
+          </Typography>
+        </Box>
+        <Box className="login-form" marginTop="2rem">
+          <Box component="form" onSubmit={handleSubmit}>
+            <TextField
+              id="email"
+              label="Email"
+              variant="outlined"
+              type="email"
+              fullWidth
+            />
+            <TextField
+              id="password"
+              label="Password"
+              variant="outlined"
+              type="password"
+              fullWidth
+              sx={{ marginTop: '2rem' }}
+            />
+            <Button
+              variant="contained"
+              fullWidth
+              sx={{ backgroundColor: '#6366F1', marginTop: '1.5rem' }}
+              aria-label="login"
+            >
+              Continue
+            </Button>
+          </Box>
+          <Typography sx={{ marginTop: '1.75rem' }}>
+            <Link
+              style={{ color: '#6366F1', cursor: 'pointer' }}
+              underline="none"
+            >
+              Forgot password?
+            </Link>
+          </Typography>
+        </Box>
+      </Paper>
+    </Box>
   );
 };
 
