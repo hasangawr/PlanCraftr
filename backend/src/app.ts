dc1d6c7bde5c249671cead7780ec394c279af65e
HasangaW <hasangawr@gmail.com>
2024-10-02 15:34:41 +0530

    Set cors options & cookie-parser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,22 @@ import express from "express";
 import authRoutes from "./routes/authRoutes";
 import connectDB from "./utils/db";
 import cors from "cors";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 connectDB();
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:5000",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Welcome to PlanCraftr!");
