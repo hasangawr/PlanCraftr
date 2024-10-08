96e2079a1962696d0c3aa02ab45baa6d9cd76c15
HasangaW <hasangawr@gmail.com>
2024-10-08 17:53:10 +0530

    Configure passport with express-session middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,14 @@ import express from "express";
 import authRoutes from "./routes/authRoutes";
 import connectDB from "./utils/db";
 import cors from "cors";
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const cookieParser = require("cookie-parser");
+import passport from "passport";
+import passportConfig from "./config/passport";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+passportConfig(passport);
 
 connectDB();
 
@@ -17,7 +21,23 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser(process.env.SESSION_SECRET));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    },
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.send("Welcome to PlanCraftr!");
