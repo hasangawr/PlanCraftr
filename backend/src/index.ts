26e22a674e6f2408f4b61666a203b0084ea88d20
HasangaW <hasangawr@gmail.com>
2024-10-09 12:21:18 +0530

    Test git hooks root level 1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,10 @@ import app from './app';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-  console.log('Test git hooks 3')
-  console.log('Test git hooks 4')
-  console.log('Test git hooks 5')
-  console.log('Test git hooks 6')
+  console.log(`Server is running on port ${PORT}`);
+  console.log('Test git hooks 3');
+  console.log('Test git hooks 4');
+  console.log('Test git hooks 5');
+  console.log('Test git hooks 6');
+  console.log('Test git hooks 7');
 });
