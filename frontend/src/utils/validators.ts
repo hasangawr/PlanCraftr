b2d9e584aa3ef75595f989bd1b89d9bdad215963
HasangaW <hasangawr@gmail.com>
2024-10-09 13:00:24 +0530

    Test git hooks root level 6

--- frontend/src/utils/validators.ts
+++ frontend/src/utils/validators.ts
@@ -8,8 +8,9 @@ export const passwordValidator = (password: string): string | boolean => {
   const minLength = 6;
   const hasNumber = /\d/.test(password);
 
+  console.log('test');
+
   if (password.length < minLength) {
-    console.log('test git hooks'); //reomve
     return `Password must be at least ${minLength} characters long`;
   }
 
