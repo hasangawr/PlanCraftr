47962c2540dd122a0c1b72128226c715f2003672
HasangaW <hasangawr@gmail.com>
2024-11-20 11:31:21 +0530

    Test staging cloudfront setup 8

--- infrastructure/terraform/staging/frontend.tf
+++ infrastructure/terraform/staging/frontend.tf
@@ -137,8 +137,8 @@ resource "aws_route53_record" "www_staging_plancraftr" {
   type    = "A"
 
   alias {
-    name = aws_s3_bucket_website_configuration.static_website_config_s3_www_staging.website_domain
-    zone_id = aws_s3_bucket.www_staging_plancraftr_com.hosted_zone_id
+    name = aws_cloudfront_distribution.www_staging_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.www_staging_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
@@ -149,8 +149,8 @@ resource "aws_route53_record" "staging_plancraftr" {
   type    = "A"
 
   alias {
-    name = aws_s3_bucket_website_configuration.static_website_config_s3_staging.website_domain
-    zone_id = aws_s3_bucket.staging_plancraftr_com.hosted_zone_id
+    name = aws_cloudfront_distribution.staging_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.staging_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
