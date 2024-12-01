7950c51729a4b27d9dbf65661f13962941346d9a
HasangaW <hasangawr@gmail.com>
2024-12-01 20:09:47 +0530

    Implement email sender

---
+++ backend/src/config/email.ts
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer';
+
+export const formatMail = (link: string) => {
+  return `<a href=${link}>Click To Verify Email</a>`;
+};
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export const sendMail = async (
+  from: string,
+  to: string,
+  subject: string,
+  html: string,
+) => {
+  const info = await transporter.sendMail({
+    from, //'"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+    to, //'bar@example.com, baz@example.com', // list of receivers
+    subject, //'Hello ✔', // Subject line
+    text: 'Hello world?', // plain text body
+    html, //'<b>Hello world?</b>', // html body
+  });
+
+  console.log('message sent: ', info.messageId);
+};
