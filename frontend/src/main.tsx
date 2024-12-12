173f99f349f8f36bc936665e9dac4d5d4274af6f
HasangaW <hasangawr@gmail.com>
2024-12-12 15:38:50 +0530

    Create basic mui theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { EmailProvider } from './contexts/EmailProvider.tsx';
+import { ThemeProvider } from '@mui/material';
+import mainTheme from './themes/MainTheme.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <EmailProvider>
-    <App />
-  </EmailProvider>,
+  <ThemeProvider theme={mainTheme}>
+    <EmailProvider>
+      <App />
+    </EmailProvider>
+  </ThemeProvider>,
 );
