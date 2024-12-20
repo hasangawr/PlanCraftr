a5e1c9d1151e12f9cdf33a975baf6ce8badef35a
HasangaW <hasangawr@gmail.com>
2024-12-20 10:57:25 +0530

    Add confirm password 7 validations to register form

--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -2,7 +2,15 @@ import {
   Box,
   Button,
   Checkbox,
+  CircularProgress,
+  Divider,
+  FormControl,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
   Link,
+  OutlinedInput,
   Paper,
   TextField,
   Typography,
@@ -18,62 +26,100 @@ import {
 } from '../utils/validators';
 import AlertSnackBar from './AlertSnackBar';
 import NotesRoundedIcon from '@mui/icons-material/NotesRounded';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import GoogleLoginButton from './GoogleLoginButton';
 
 const RegisterForm = () => {
   const [name, setName] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [showConfirmPassword, setShowConfirmPassword] =
+    useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
   const [termsChecked, setTermsChecked] = useState<boolean>(false);
   const [nameError, setNameError] = useState<boolean | string>(false);
   const [emailError, setEmailError] = useState<boolean | string>(false);
   const [passwordError, setPasswordError] = useState<boolean | string>(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState<
+    boolean | string
+  >(false);
   const [termsError, setTermsError] = useState<boolean | string>(false);
-  const [alertOpen, setAlertOpen] = useState<boolean>(false);
+  const [alertOpen, setAlertOpen] = useState<boolean | null>(false);
   const [alertMessage, setAlertMessage] = useState<string>('');
+  const [passwordMismatchError, setPasswordMismatchError] =
+    useState<boolean>(false);
+  const [sendingRequest, setSendingRequest] = useState<boolean>(false);
 
   const navigate = useNavigate();
 
   const theme = useTheme();
 
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleClickShowConfirmPassword = () =>
+    setShowConfirmPassword((show) => !show);
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!name || !email || !password || !termsChecked) {
-      if (!name) setNameError('Name is required');
-      if (!email) setEmailError('Email is required');
-      if (!password) setPasswordError('Password is required');
-      if (!termsChecked) setTermsError('You need to accept Terms & Conditions');
-      return;
-    }
+    if (document.activeElement?.ariaLabel === 'Register') {
+      if (!name || !email || !password || !termsChecked) {
+        if (!name) setNameError('Name is required');
+        if (!email) setEmailError('Email is required');
+        if (!password) setPasswordError('Password is required');
+        if (!termsChecked)
+          setTermsError('You need to accept Terms & Conditions');
+        return;
+      }
 
-    if (!nameError && !emailError && !passwordError && termsChecked) {
-      try {
-        const response = await axios.post(
-          `${import.meta.env.VITE_API}/auth/register`,
-          { name, email, password },
-          { withCredentials: true },
-        );
+      if (
+        !nameError &&
+        !emailError &&
+        !passwordError &&
+        termsChecked &&
+        !passwordMismatchError
+      ) {
+        try {
+          setSendingRequest(true);
 
-        console.log('Registration response: ', response);
+          const response = await axios.post(
+            `${import.meta.env.VITE_API}/auth/register`,
+            { name, email, password },
+            { withCredentials: true },
+          );
 
-        if (response.status === 201 && response.data.email) {
-          setName('');
-          setEmail('');
-          setPassword('');
-          navigate('/verify-email');
-        } else {
-          setAlertMessage(response.data.message);
+          if (response.status === 201 && response.data.email) {
+            setSendingRequest(false);
+            setName('');
+            setEmail('');
+            setPassword('');
+            navigate('/verify-email');
+          } else {
+            setSendingRequest(false);
+            setAlertMessage(response.data.message);
+            setAlertOpen(true);
+          }
+        } catch (error) {
+          setSendingRequest(false);
+          console.log('Registration failed: ', error);
+          setAlertMessage('Registration failed. Try again');
           setAlertOpen(true);
         }
-      } catch (error) {
-        console.log('Registration failed: ', error);
-        setAlertMessage('Registration failed. Try again');
-        setAlertOpen(true);
       }
     }
   };
 
-  return (
+  return sendingRequest ? (
+    <Box
+      sx={{
+        display: 'flex',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  ) : (
     <Box
       sx={{
         height: '100vh',
@@ -89,6 +135,7 @@ const RegisterForm = () => {
         displayDuration={5000}
         severity="error"
         message={alertMessage}
+        position={{ vertical: 'bottom', horizontal: 'left' }}
       />
       <Paper
         elevation={3}
@@ -158,20 +205,102 @@ const RegisterForm = () => {
               error={emailError ? true : false}
               helperText={emailError}
             />
-            <TextField
-              id="password"
-              label="Password"
-              variant="outlined"
-              type="password"
-              fullWidth
-              sx={{ marginTop: '1rem' }}
-              onChange={(e) => {
-                setPasswordError(passwordValidator(e.target.value));
-                setPassword(e.target.value);
-              }}
-              error={passwordError ? true : false}
-              helperText={passwordError}
-            />
+
+            <FormControl fullWidth>
+              {/* password */}
+              <InputLabel
+                htmlFor="outlined-adornment-password"
+                variant="outlined"
+                sx={{ marginTop: '1rem' }}
+                error={passwordError ? true : false}
+              >
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label={
+                        showPassword
+                          ? 'hide the password'
+                          : 'display the password'
+                      }
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+                sx={{ marginTop: '1rem' }}
+                fullWidth
+                onChange={(e) => {
+                  setPasswordError(passwordValidator(e.target.value));
+                  setPassword(e.target.value);
+                }}
+                error={passwordError ? true : false}
+              />
+              {passwordError && (
+                <FormHelperText error={true}>{passwordError}</FormHelperText>
+              )}
+            </FormControl>
+
+            {/* confirm password */}
+            <FormControl fullWidth>
+              <InputLabel
+                htmlFor="outlined-adornment-confirm-password"
+                variant="outlined"
+                sx={{ marginTop: '1rem' }}
+                error={confirmPasswordError ? true : false}
+              >
+                Confirm Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-confirm-password"
+                type={showConfirmPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label={
+                        showConfirmPassword
+                          ? 'hide the password'
+                          : 'display the password'
+                      }
+                      onClick={handleClickShowConfirmPassword}
+                      edge="end"
+                    >
+                      {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Confirm Password"
+                sx={{ marginTop: '1rem' }}
+                fullWidth
+                onChange={(e) => {
+                  setConfirmPasswordError(passwordValidator(e.target.value));
+                  if (e.target.value !== password) {
+                    setPasswordMismatchError(true);
+                  } else {
+                    setPasswordMismatchError(false);
+                  }
+                }}
+                error={confirmPasswordError ? true : false}
+              />
+              {confirmPasswordError && (
+                <FormHelperText error={true}>
+                  {confirmPasswordError}
+                </FormHelperText>
+              )}
+              {passwordMismatchError && (
+                <FormHelperText error={true}>
+                  Passwords do not match
+                </FormHelperText>
+              )}
+            </FormControl>
+
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
               <Checkbox
                 checked={termsChecked}
@@ -204,11 +333,10 @@ const RegisterForm = () => {
               Register
             </Button>
 
-            {/*TODO: Add this without validation errors*/}
-            {/* <Divider>
+            <Divider>
               <Typography>or</Typography>
             </Divider>
-            <GoogleLoginButton /> */}
+            <GoogleLoginButton />
           </Box>
         </Box>
       </Paper>
